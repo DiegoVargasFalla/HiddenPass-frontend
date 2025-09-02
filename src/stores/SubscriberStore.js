885e7e5db4cc90ab2c872d28ccc7da29f0445c72
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-09-02 01:29:30 -0300

    update store

--- src/stores/SubscriberStore.js
+++ src/stores/SubscriberStore.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { defineStore } from "pinia";
+import { useShowLayerPopsUp } from "@/modules/dashboard/store/layerPopsUpStore";
 
-
-export const subscriberStore = defineStore('subscriberStore', {
+export const useSubscriberStore = defineStore('subscriberStore', {
     state: () => ({
-        subscriberEmail: ''
+        subscriberEmail: '',
+        showPopUp: false,
+        showCheckIconSubcriber: false
     }),
     actions: {
         getSubcriberEmail() {
@@ -14,10 +16,23 @@ export const subscriberStore = defineStore('subscriberStore', {
             this.subscriberEmail = value;
         },
         async subcribeUser() {
+            const subcriberStore = useSubscriberStore();
+            const layerPopsUpStore = useShowLayerPopsUp();
             try {
-                const response = axios.post('',
-                    
+                const response = await axios.post('/api/v1/subscriber',
+                    {email: this.subscriberEmail}
                 )
+
+                const dada = response.data;
+                if(dada) {
+                    subcriberStore.showCheckIconSubcriber = true;
+                    setTimeout(() => {
+                        layerPopsUpStore.setShowLayerPopsUp(false);
+                        this.showPopUp = false;
+                        this.subscriberEmail = "";
+                        subcriberStore.showCheckIconSubcriber = false;
+                    }, 2000);
+                }
             } catch(error) {
                 console.log();
             }
